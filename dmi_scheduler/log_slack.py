ec3effe4f84b9a107ddba0b4f01fabd468fecbc6
Dale Wahl <dalewahl@gmail.com>
2022-04-20 14:11:52 +0200

    try fix slack error messages again, probably fail.

--- dmi_scheduler/log_slack.py
+++ dmi_scheduler/log_slack.py
@@ -91,8 +91,7 @@ class SlackFormatter(Formatter):
 
     def format(self, record):
         record.message = super(SlackFormatter, self).format(record)
-        self.attachment['text'] = self.attachment['text'] % record.__dict__
-        json_string = json.dumps(self.attachment)
+        json_string = json.dumps(self.attachment % record.__dict__)
 
         attachment = json.loads(json_string, strict=False)
         attachment.update({'color': self.level_to_color[record.levelname]})
